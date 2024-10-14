d9fb58f47c2ac22350fe5cda41f5c77bac06da50
Gino Ocampo <gocampo@cliodinamica.com>
2024-10-14 10:44:28 -0300

    MasterFile
    MasterFile y títulos

---
+++ ENADEL 2023/Masterfile.R
@@ -0,0 +1,17 @@
+###MASTERFILE###
+
+#Cargar paquetes
+pacman::p_load(tidyverse, haven, summarytools,quarto)
+
+
+#3_armado_bbdd---------------------------------------------------------------
+source(Scripts/"3_armado_bbdd.R")
+
+#4_pivot
+source(Scripts/"4_pivot.R")
+
+#5_analisis--------------------------------------------------------------------
+source("5_analisis.R")
+
+#6 Renderizar Reporte Quarto
+quarto::quarto_render("Reporte.qmd")
